56750fb9ec87b48ddc30898d05675d4406a68215
ariericyron <ariericyron@gmail.com>
2023-10-02 19:49:06 -0400

    feat:modified my models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,4 @@
 class Comment < ApplicationRecord
+include Visible
   belongs_to :article
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
